3276237c53953b30f2e15b9c4e81bbf77c1aef7f
Cthulhu Ftaghn <cthulhu@ryleh.example.com>
2020-11-17 19:17:29 -0800

    Trying to get rid of r7rs dependencies

--- srfi-136.scm
+++ srfi-136.scm
@@ -2,8 +2,7 @@
 (import (except (chicken base) define-record-type))
 (import (chicken platform))
 (import (srfi-99))
-(import r7rs)
 
 (register-feature! 'srfi-136)
 
-(include "srfi/136.sld")
+(include "srfi/136.scm")
